c1edcbfa755def71b74cd7b1acdb03c2d17f189a
YonY18 <yonap80@gmail.com>
2022-04-16 15:00:20 -0300

    filter_v1.5

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -26,7 +26,79 @@ function rootReducer (state = initialState, action){
                 ...state,
                 pokemons: statusFiltered.length ? statusFiltered : [`${action.payload} Pokemons`]
             }
-
+        case "ORDER_BY_NAME_OR_STRENGH":
+            let sortedArray
+    
+            if(action.payload === 'asc'){
+                    sortedArray = state.pokemons.sort(function (a, b){
+                        if(a.name > b.name){
+                            return 1;
+                        }
+                        if(b.name > a.name){
+                            return -1;
+                        }
+                            return 0;
+                        }) 
+            }
+            if(action.payload === 'desc'){
+                    sortedArray = state.pokemons.sort(function (a, b){
+                        if(a.name > b.name){
+                            return -1;
+                        }
+                        if(b.name > a.name){
+                            return 1;
+                        }
+                        return 0;
+                        }) 
+                }
+            if(action.payload === 'HAttack'){
+                    sortedArray = state.pokemons.sort(function (a, b){
+                        if(a.attack > b.attack){
+                            return -1;
+                        }
+                        if(b.attack > a.attack){
+                            return 1;
+                        }
+                        return 0;
+                        }) 
+                }
+            if(action.payload === 'LAttack'){
+                    sortedArray = state.pokemons.sort(function (a, b){
+                        if(a.attack > b.attack){
+                            return 1;
+                        }
+                        if(b.attack > a.attack){
+                            return -1;
+                        }
+                        return 0;
+                        }) 
+                }  
+            if(action.payload === 'normal'){
+                const apiPokes = state.pokemons.filter( el => !el.createdInDb).sort(function (a, b){
+                        if(a.id > b.id){
+                            return 1;
+                        }
+                        if(b.id > a.id){
+                            return -1;
+                        }
+                        return 0;
+                        }) 
+                const dbPokes = state.pokemons.filter( el => el.createdInDb).sort(function (a, b){
+                        if(a.id > b.id){
+                            return 1;
+                        }
+                        if(b.id > a.id){
+                            return -1;
+                        }
+                        return 0;
+                    })
+                    sortedArray = [...apiPokes, ...dbPokes]
+                }  
+                return {
+                    ...state,
+                    pokemons: sortedArray
+                }  
+    
             
 
             default: 
